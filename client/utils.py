6e95002ced5748e34b058f80e98b3a724e9cda60
overtrekwolf <overtrek.dev@gmail.com>
2025-04-29 16:49:30 +0300

    more fix

--- client/utils.py
+++ client/utils.py
@@ -9,6 +9,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 CONFIG_PATH = os.path.join(BASE_DIR, "config.yaml")
 
+
 def load_config():
     with open(CONFIG_PATH, "r") as f:
         return yaml.safe_load(f)
